0e09ffc4a1396f85ba324ab43fd52b9ec8473d8d
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-11-06 05:27:22 +0200

    feat: :clown_face: added mock data for users list

---
+++ utils/mocks.ts
@@ -0,0 +1,18 @@
+import { UserList } from "../types/userTypes";
+
+export const userListMock : UserList= {
+    users: [
+        {
+            id: 1,
+            name: "Alice",
+            email: "alice@example.com",
+            password: "1234"
+        },
+        {
+            id: 2,
+            name: "Bob",
+            email: "Bob@example.com",
+            password: "5678"
+        },
+    ]
+}
