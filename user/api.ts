6dddf3d7b9b5fd4b960696b876c79fac50b8cc58
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-11-13 13:39:30 +0200

    feat: :sparkles: added Database connection, ORM integration and querying all users for getUsers endpoint

--- user/api.ts
+++ user/api.ts
@@ -1,11 +1,28 @@
 import { api } from "encore.dev/api";
 import { User, UserList } from "./types/userTypes";
 import { userListMock } from "./utils/mocks";
+import { IRootResponse } from "./types/responseTypes";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
+import knex from "knex";
+
+const db = new SQLDatabase("encore_users_db", {migrations: "./migrations"})
+
+const orm = knex({
+    client: "pg",
+    connection: db.connectionString,
+  });
+
+const usersTable = () => orm<User>("user_table");
 
 export const getUsers = api({method: "GET", path: "/users"}, async () : Promise<UserList> => {
-    return userListMock
+    const usersData = await usersTable().select()
+    return {users : usersData}
 })
 
 export const createUser = api({method: "POST", path: "/user"}, async(newUser: User) : Promise<User> => {
     return newUser
+})
+
+export const rootRoute = api({method: "GET", path: "/api"}, async () : Promise<IRootResponse> => {
+    return {message : "Welcome to Encore.ts"}
 })
