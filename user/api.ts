772e801d806075d5089e3adf67c8d48e2358e1dc
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-11-14 09:39:17 +0200

    feat: :goal_net: Error handling added with correct http error codes

--- user/api.ts
+++ user/api.ts
@@ -1,4 +1,4 @@
-import { api } from "encore.dev/api";
+import { api, APIError } from "encore.dev/api";
 import { InsertUser, OptionalUser, PartialUser, User, UserList } from "./types/userTypes";
 import { userListMock } from "./utils/mocks";
 import { IRootResponse } from "./types/responseTypes";
@@ -18,30 +18,46 @@ const usersTable = () => orm<User>("user_table");
 
 export const getUsers = api({method: "GET", path: "/users"}, async () : Promise<UserList> => {
     const usersData = await usersTable().select()
+    if (usersData.length === 0) {
+        throw APIError.notFound("No users found")
+    }
     return {users : usersData}
 })
 
 export const createUser = api({method: "POST", path: "/user"}, async(newUser: InsertUser) : Promise<User> => {
+    const [existingUser] = await usersTable().where("username", newUser.username)
+    if (existingUser) {
+        throw APIError.alreadyExists("User already exists")
+    }
     const [newUserResponse] = await usersTable().insert(newUser).returning("*")
+    if (!newUserResponse) {
+        throw APIError.internal("Failed to create user")
+    }
     return newUserResponse 
 })
 
 export const updateUser = api({method: "PATCH", path: "/user/:username"}, async({username, newUserData} : IUpdateUserRequest) : Promise<User> => {
     if (username == "") {
-        throw new Error("username is required")
+        throw APIError.invalidArgument("username is required")
     }
     if (newUserData && Object.keys(newUserData).length === 0) {
-        throw new Error("At least one field is required")
+        throw APIError.invalidArgument("At least one field is required")
     }
     const [updatedUser] = await usersTable().where("username", username).update(newUserData).returning("*")
+    if (!updatedUser) {
+        throw APIError.notFound("User not found")
+    }
     return updatedUser
 })
 
 export const deleteUser = api({method: "DELETE", path: "/user/:username"}, async({username} : {username: string}) : Promise<User> => {
     if (username == "") {
-        throw new Error("username is required")
+        throw APIError.invalidArgument("username is required")
     }
     const [deletedUser] = await usersTable().where("username", username).del().returning("*")
+    if (!deletedUser) {
+        throw APIError.notFound("User not found")
+    }
     return deletedUser
 })
 
@@ -51,5 +67,8 @@ export const rootRoute = api({method: "GET", path: "/api"}, async () : Promise<I
 
 export const getUser = api({method: "GET", path: "/user/:username"}, async({username} : {username: string}) : Promise<User> => {
     const [user] = await usersTable().where("username", username)
+    if (!user) {
+        throw APIError.notFound("User not found")
+    }
     return user
 })
