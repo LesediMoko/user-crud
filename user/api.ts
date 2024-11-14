db9e780ce1efcb8a6c5c3745a9d8a47f6105ea6b
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-11-14 09:47:53 +0200

    feat: :goal_net: added logic to catch all internal server errors

--- user/api.ts
+++ user/api.ts
@@ -6,6 +6,7 @@ import { SQLDatabase } from "encore.dev/storage/sqldb";
 import knex from "knex";
 import { RequireAtLeastOne } from "./types/genericTypes";
 import { IUpdateUserRequest } from "./types/requestTypes";
+import { Logger } from "encore.dev/log";
 
 const db = new SQLDatabase("encore_users_db", {migrations: "./migrations"})
 
@@ -17,7 +18,9 @@ const orm = knex({
 const usersTable = () => orm<User>("user_table");
 
 export const getUsers = api({method: "GET", path: "/users"}, async () : Promise<UserList> => {
-    const usersData = await usersTable().select()
+    const usersData = await usersTable().select().catch(() => {
+        throw APIError.internal
+    })
     if (usersData.length === 0) {
         throw APIError.notFound("No users found")
     }
@@ -25,11 +28,15 @@ export const getUsers = api({method: "GET", path: "/users"}, async () : Promise<
 })
 
 export const createUser = api({method: "POST", path: "/user"}, async(newUser: InsertUser) : Promise<User> => {
-    const [existingUser] = await usersTable().where("username", newUser.username)
+    const [existingUser] = await usersTable().where("username", newUser.username).catch(() => {
+        throw APIError.internal
+    })
     if (existingUser) {
         throw APIError.alreadyExists("User already exists")
     }
-    const [newUserResponse] = await usersTable().insert(newUser).returning("*")
+    const [newUserResponse] = await usersTable().insert(newUser).returning("*").catch(() => {
+        throw APIError.internal
+    })
     if (!newUserResponse) {
         throw APIError.internal("Failed to create user")
     }
@@ -43,7 +50,9 @@ export const updateUser = api({method: "PATCH", path: "/user/:username"}, async(
     if (newUserData && Object.keys(newUserData).length === 0) {
         throw APIError.invalidArgument("At least one field is required")
     }
-    const [updatedUser] = await usersTable().where("username", username).update(newUserData).returning("*")
+    const [updatedUser] = await usersTable().where("username", username).update(newUserData).returning("*").catch(() => {
+        throw APIError.internal
+    })
     if (!updatedUser) {
         throw APIError.notFound("User not found")
     }
@@ -54,7 +63,9 @@ export const deleteUser = api({method: "DELETE", path: "/user/:username"}, async
     if (username == "") {
         throw APIError.invalidArgument("username is required")
     }
-    const [deletedUser] = await usersTable().where("username", username).del().returning("*")
+    const [deletedUser] = await usersTable().where("username", username).del().returning("*").catch(() => {
+        throw APIError.internal
+    })
     if (!deletedUser) {
         throw APIError.notFound("User not found")
     }
@@ -66,7 +77,9 @@ export const rootRoute = api({method: "GET", path: "/api"}, async () : Promise<I
 })
 
 export const getUser = api({method: "GET", path: "/user/:username"}, async({username} : {username: string}) : Promise<User> => {
-    const [user] = await usersTable().where("username", username)
+    const [user] = await usersTable().where("username", username).catch(() => {
+        throw APIError.internal
+    })
     if (!user) {
         throw APIError.notFound("User not found")
     }
